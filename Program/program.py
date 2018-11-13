9442e1019dcc15b4f909cf9f2209d08d7013ad95
736659711@qq.com <736659711@qq.com>
2018-11-13 20:36:46 +0800

    语法错误稍微处理

--- Program/program.py
+++ Program/program.py
@@ -18,31 +18,49 @@ def analysize(input_str):
     pos = 0
     while pos < len(input_str):
         ch = input_str[pos]
-        print('当前字符:' + ch)
-        print('剩下字符' + input_str[pos:])
-        res = rule[symbolStack[-1]][ch]
-        print('识别的结果:' + res)
-        if res:
+        # print('当前字符:' + ch)
+        # print('剩下字符' + input_str[pos:])
+        if ch in rule[symbolStack[-1]]:
+            res = rule[symbolStack[-1]][ch]
+            # print('识别的结果:' + res)
             symbolStack = symbolStack[:-1] + res[::-1][:-3]
-            print('符号栈:' + symbolStack)
+            # print('符号栈:' + symbolStack)
             while symbolStack[-1] == ch:
                 symbolStack = symbolStack[:-1]
-                print('符号栈:' + symbolStack)
+                # print('符号栈:' + symbolStack)
                 pos += 1
                 if pos >= len(input_str):
                     break
                 ch = input_str[pos]
+            if symbolStack != '' and symbolStack[-1] not in rule:
+                # print(input_str[pos] + '终结符匹配失败')
+                dealError(1,input_str[pos],pos, symbolStack[-1])
+                break
         else:
-            print('error')
+            # print('非终结符里没有这个去处')
+            dealError(2,input_str[pos],pos,symbolStack[-1],rest = input_str[pos:])
+            break
     if symbolStack == '':
-        print('成功')
-    else:
-        print('error')
+        print('识别成功')
+
+# 类别分析
+dic = {'NUM':'a','ID':'b','if':'c','else':'d','for':'e','while':'f','int':'g','write':'h','read':'i','(':'j',')':'k',';':'l','{':'m','}':'n',',':'o','+':'p','-':'q','*':'r','/':'s','=':'t','>':'u','<':'v','>=':'w','<=':'x','!=':'y','==':'z','注释':1,'#':'#'}
+dic = dict(zip(dic.values(),dic.keys()))
+
+def dealError(code,now,pos, need, rest = ''):
+    if code == 1:
+        print('Expected:    ' + dic[need] + '   Before:    ' + map_list[pos] )
+    elif code == 2:
+        s = ''
+        print(need + ' 产生式无法产生 ' + dic[now])
+        print('剩余未识别' + rest)
 
 
-def pro(input_str):
+def pro(input_str, m_list):
     global rule
     global startSymbol
+    global map_list
+    map_list = m_list
     rule, startSymbol = readRules()
     analysize(input_str)
 
--- run.py
+++ run.py
@@ -5,7 +5,11 @@ from Program.program import pro
 if __name__ == '__main__':
     lex('test.txt')
     input_str = ''
+    map_list = list()
     with open('lex.txt') as f:
         for line in f.readlines():
-            input_str += line[0]
-    pro(input_str+'#')
+            line = line[:-1].split('\t')
+            if line[0].isalpha():
+                input_str += line[0]
+                map_list.append(line[1])
+    pro(input_str + '#', map_list)
